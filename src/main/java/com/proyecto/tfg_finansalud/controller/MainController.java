bc4f5219cfeeaa1f523fc3177d0b47e31cf78bbf
paolo <paoloIdrugo84@gmail.com>
2025-06-06 20:44:43 +0200

    env

--- src/main/java/com/proyecto/tfg_finansalud/controller/MainController.java
+++ src/main/java/com/proyecto/tfg_finansalud/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.proyecto.tfg_finansalud.controller;
 
+import com.proyecto.tfg_finansalud.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+    UserService userService;
+
     @GetMapping({"/login","/"})
     public String login() {
         return "/user_login/user_login";
